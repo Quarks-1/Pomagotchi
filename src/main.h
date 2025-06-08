3fc0a22085ff6e3900db79a225814dfb58e183c6
Quarks-1 <riopacheco7@gmail.com>
2025-06-08 02:09:34 -0400

    add

---
+++ src/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// Global debug mode flag
+extern bool isDebugMode;
+
+// Function to check if debug mode is active
+bool isDebugModeActive();
+
+#endif // MAIN_H 
