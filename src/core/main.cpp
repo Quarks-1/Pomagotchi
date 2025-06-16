a02cd3e406b7657fbcd1fab9029ce4711a096241
Quarks-1 <riopacheco7@gmail.com>
2025-06-15 23:30:11 -0400

    Update depletion, turn off debug

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -33,7 +33,7 @@
 GxEPD2_BW<GxEPD2_154_D67, GxEPD2_154_D67::HEIGHT> display(GxEPD2_154_D67(EPD_CS_PIN, EPD_DC_PIN, EPD_RST_PIN, EPD_BUSY_PIN));
 Page currentPage = HOME_PAGE;
 Page previousPage = HOME_PAGE;  // Track previous page for detecting changes
-bool isDebugMode = true;  // Global debug mode flag
+bool isDebugMode = false;  // Global debug mode flag
 bool isEncoderEnabled = true;  // Global flag to enable/disable encoder functionality
 bool isLightSensorEnabled = true; // Global flag to enable/disable light sensor functionality
 
--- src/pet/depletion.h
+++ src/pet/depletion.h
@@ -3,9 +3,8 @@
 #include <Arduino.h>
 
 // Constants for depletion rates
-constexpr unsigned long SUNLIGHT_DEPLETION_INTERVAL = 2000;     // 1 second in milliseconds
-// constexpr unsigned long SUNLIGHT_DEPLETION_INTERVAL = 7200000;  // 2 hours in milliseconds (commented out for now)
-constexpr unsigned long THIRST_DEPLETION_INTERVAL = 2000;        // 500ms for thirst
+constexpr unsigned long SUNLIGHT_DEPLETION_INTERVAL = 4320000;     // 1.2 hours in milliseconds (depletes 100 units in 5 days)
+constexpr unsigned long THIRST_DEPLETION_INTERVAL = 4320000;       // 1.2 hours in milliseconds (depletes 100 units in 5 days)
 constexpr unsigned long PET_STATUS_DEPLETION_INTERVAL = 7200000;  // 2 hours in milliseconds
 
 // Function declarations
